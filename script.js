3dfee4aafea418e2edcfb0dd1f79087d16bfd5ad
Nihar Jachak <109013101+niharjachak@users.noreply.github.com>
2026-02-06 21:26:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+let level = 0;
+
+// Traverse up the DOM tree
+while (element) {
+    level++;
+    element = element.parentElement;
+}
+
+alert("The level of the element is: " + level);
